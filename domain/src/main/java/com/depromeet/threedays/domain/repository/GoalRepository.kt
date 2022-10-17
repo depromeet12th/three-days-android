f83597d1b9f6bf1c42cdd34a1d91be30d672a7fa
hyein <hyein2072@gmail.com>
2022-10-17 23:41:22 +0900

    feat: 목표 생성/추가/삭제를 위한 Repository Interface 작성 및 목표(goal) data class 생성

---
+++ domain/src/main/java/com/depromeet/threedays/domain/repository/GoalRepository.kt
@@ -0,0 +1,25 @@
+package com.depromeet.threedays.domain.repository
+
+import com.depromeet.threedays.domain.entity.Goal
+
+interface GoalRepository {
+    suspend fun getGoal(goalId: Int): Goal
+    suspend fun postGoal(
+        title: String,
+        startDate: String,
+        endDate: String,
+        startTime: String,
+        notificationTime: String,
+        notificationContent: String
+    )
+    suspend fun updateGoal(
+        goalId: Int,
+        title: String,
+        startDate: String,
+        endDate: String,
+        startTime: String,
+        notificationTime: String,
+        notificationContent: String
+    )
+    suspend fun deleteGoal(goalId: Int)
+}
