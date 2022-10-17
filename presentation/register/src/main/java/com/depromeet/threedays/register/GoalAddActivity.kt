56008c71fcf80eb14513193517a163af902a74ff
hyeinKim <68214704+sdu07024@users.noreply.github.com>
2022-10-17 20:10:53 +0900

    DEP-54 feat: 목표 생성 페이지 그리기 (#18)
    * feat: register(목표 등록) 모듈 생성
    
    * feat: register 모듈 build.gradle 설정
    
    * ui: 목표 등록 화면 뷰 제작
    
    * ui: 수행 시간 default 문구 추가
    
    * fix: 취약성 문제 해결을 위해 exported 값을 false로 변경

---
+++ presentation/register/src/main/java/com/depromeet/threedays/register/GoalAddActivity.kt
@@ -0,0 +1,11 @@
+package com.depromeet.threedays.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoalAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_add)
+    }
+}
