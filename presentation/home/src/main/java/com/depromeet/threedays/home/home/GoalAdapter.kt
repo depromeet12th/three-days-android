69e2767acd63105566712b03afc49250210ac50a
Juhwan Kim <juhwan.dev@gmail.com>
2022-10-19 16:24:22 +0900

    DEP-50 feat: 목표 목록 기능 구현 (#20)
    * wip: 하는...중...
    
    * ui: 목표 목록 ui 데이터 바인딩 연결
    
    * feat: 목표 목록 adapter & recyclerView 구현
    
    * fix: 필요하지 않은 id 삭제

---
+++ presentation/home/src/main/java/com/depromeet/threedays/home/home/GoalAdapter.kt
@@ -0,0 +1,34 @@
+package com.depromeet.threedays.home.home
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+class GoalAdapter : ListAdapter<Goal, GoalViewHolder>(DIFF_UTIL) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalViewHolder {
+        return GoalViewHolder.create(parent, false)
+    }
+
+    override fun onBindViewHolder(holder: GoalViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    override fun getItemCount(): Int = currentList.size
+
+    override fun getItemId(position: Int): Long {
+        return getItem(position).id.toLong()
+    }
+
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<Goal>() {
+            override fun areItemsTheSame(oldItem: Goal, newItem: Goal): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Goal, newItem: Goal): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
