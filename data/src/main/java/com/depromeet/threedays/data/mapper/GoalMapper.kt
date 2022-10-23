4387d85fc2b395bb140095d97bd5281d96ad9e4c
hyein <hyein2072@gmail.com>
2022-10-23 19:00:29 +0900

    fix: Goal 데이터 클래스 변경

--- data/src/main/java/com/depromeet/threedays/data/mapper/GoalMapper.kt
+++ data/src/main/java/com/depromeet/threedays/data/mapper/GoalMapper.kt
@@ -2,6 +2,8 @@ package com.depromeet.threedays.data.mapper
 
 import com.depromeet.threedays.data.entity.GoalEntity
 import com.depromeet.threedays.domain.entity.Goal
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 fun Goal.toGoalEntity(): GoalEntity {
     return GoalEntity(
@@ -13,13 +15,9 @@ fun Goal.toGoalEntity(): GoalEntity {
 fun GoalEntity.toGoal() = Goal(
     goalId = this.goalId,
     title = this.title,
-    startDate = "",
-    endDate = "",
-    startTime = "",
-    notificationTime = "",
+    startDate = ZonedDateTime.now(ZoneId.systemDefault()),
+    endDate = ZonedDateTime.now(ZoneId.systemDefault()),
+    startTime = ZonedDateTime.now(ZoneId.systemDefault()),
+    notificationTime = ZonedDateTime.now(ZoneId.systemDefault()),
     notificationContent = "",
-    status = "",
-    createDate = "",
-    sequence = "",
-    lastAchievementDate = "",
 )
