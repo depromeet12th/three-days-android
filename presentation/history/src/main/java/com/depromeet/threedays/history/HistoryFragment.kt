3e8e27eca48baf9643fcda5f0bcd9743f841aed3
juhwankim-dev <juhwan.dev@gmail.com>
2022-11-26 12:30:22 +0900

    feat: 툴팁 기능 구현

--- presentation/history/src/main/java/com/depromeet/threedays/history/HistoryFragment.kt
+++ presentation/history/src/main/java/com/depromeet/threedays/history/HistoryFragment.kt
@@ -1,7 +1,10 @@
 package com.depromeet.threedays.history
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.depromeet.threedays.core.BaseFragment
+import com.depromeet.threedays.core.extensions.switchVisible
 import com.depromeet.threedays.history.databinding.FragmentHistoryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,4 +12,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class HistoryFragment: BaseFragment<FragmentHistoryBinding, HistoryViewModel>(R.layout.fragment_history) {
     override val viewModel by viewModels<HistoryViewModel>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initEvent()
+    }
+
+    private fun initEvent() {
+        binding.ivTooltip.setOnClickListener {
+            binding.cvTooltip.switchVisible()
+        }
+    }
 }
