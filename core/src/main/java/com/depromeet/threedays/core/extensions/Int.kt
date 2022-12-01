bdde970b4e65258cb94f48748a6a442f15ee3a73
hyein <hyein2072@gmail.com>
2022-12-01 23:17:42 +0900

    feat: 이모지를 가져오는 함수 생성

--- core/src/main/java/com/depromeet/threedays/core/extensions/Int.kt
+++
@@ -1,4 +0,0 @@
-package com.depromeet.threedays.core.extensions
-
-val Int?.toEmoji
-    get() = String(Character.toChars(this ?: 0))
--- core/src/main/java/com/depromeet/threedays/core/util/Emoji.kt
+++ core/src/main/java/com/depromeet/threedays/core/util/Emoji.kt
@@ -5,4 +5,16 @@ class Emoji{
         "trash" to 0x1F5D1,
         "clock" to 0x23F0,
     )
+
+    fun getEmojiString(code: Int?): String {
+        if (code != null) {
+            return String(Character.toChars(code))
+        } else {
+            throw Exception()
+        }
+    }
+
+    fun getEmojiString(word: String): String {
+        return getEmojiString(objects[word])
+    }
 }
