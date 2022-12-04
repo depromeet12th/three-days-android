227f90f5bc61d2ff944e3460f59c963dd8ca4330
hyein <hyein2072@gmail.com>
2022-12-04 16:49:30 +0900

    feat: 캘린더 제외 상세기록 UI 제작 및 컬러명/폰트명 변경

---
+++ presentation/history/src/main/java/com/depromeet/threedays/history/detail/DetailHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.depromeet.threedays.history.detail
+
+import android.os.Bundle
+import com.depromeet.threedays.core.BaseActivity
+import com.depromeet.threedays.history.R
+import com.depromeet.threedays.history.databinding.ActivityDetailHistoryBinding
+
+class DetailHistoryActivity : BaseActivity<ActivityDetailHistoryBinding>(R.layout.activity_detail_history) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
