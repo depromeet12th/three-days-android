f4f777dc9c132012647cbad931112aa654298ec5
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-05 02:07:34 +0900

    feat: 짝꿍 만들기 3단계 fragment와 viewModel 생성

---
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/create/step3/SetMateNicknameViewMoodel.kt
@@ -0,0 +1,11 @@
+package com.depromeet.threedays.mate.create.step3
+
+import com.depromeet.threedays.core.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SetMateNicknameViewMoodel @Inject constructor(
+) : BaseViewModel() {
+
+}
