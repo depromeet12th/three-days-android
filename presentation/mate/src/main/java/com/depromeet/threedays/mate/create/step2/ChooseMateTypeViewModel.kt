47861b6642469171608a6ce72991150ae3be6d36
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-06 00:57:24 +0900

    feat: 짝꿍 선택 상태를 주고 받는 fragment와 viewModel 구현

--- presentation/mate/src/main/java/com/depromeet/threedays/mate/create/step2/ChooseMateTypeViewModel.kt
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/create/step2/ChooseMateTypeViewModel.kt
@@ -2,10 +2,33 @@ package com.depromeet.threedays.mate.create.step2
 
 import com.depromeet.threedays.core.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class ChooseMateTypeViewModel @Inject constructor(
+
 ) : BaseViewModel() {
 
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState>
+        get() = _uiState
+
+    fun setMateType(whipping: MateType) {
+        _uiState.update {
+            it.copy(
+                mateType = whipping
+            )
+        }
+    }
+
+    data class UiState(
+        val mateType: MateType = MateType.WHIPPING
+    )
+
+    enum class MateType {
+        WHIPPING, CARROT
+    }
 }
