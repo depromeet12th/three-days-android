5ab12b19619e6d77666f75492fde14e559f8c5cb
hyein-kim <68214704+kimhyeing@users.noreply.github.com>
2022-12-09 18:14:40 +0900

    DEP-188 ui: 캘린더 제외 상세 기록 페이지 UI 제작 (#56)
    * feat: 캘린더 제외 상세기록 UI 제작 및 컬러명/폰트명 변경
    
    * refactor: string 값 추출
    
    * feat: 상세 기록 페이지의 캘린더 구현
    
    * ui: 아이콘 변경사항 적용
    
    * ui: 스크롤 적용
    
    * chore: 코드가 날아가서 다시 복구
    
    * ui: android:text -> tools:text로 변경
    
    * feat: 뒤로가기 시 액션과 캘린더 스크롤 시 다음 달로 넘기는 버튼의 enable 설정
    
    * fix: AndroidManifest.xml 에 상세기록 activity 추가
    
    * refactor: import 추가하여 코드 길이 줄이기

--- core/src/main/java/com/depromeet/threedays/core/extensions/View.kt
+++ core/src/main/java/com/depromeet/threedays/core/extensions/View.kt
@@ -47,5 +47,3 @@ inline fun <reified T : ViewGroup.LayoutParams> View.layoutParams(block: T.() ->
 }
 
 fun View.dpToPx(dp: Float): Int = context.dpToPx(dp)
-
-fun Context.dpToPx(dp: Float): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics).toInt()
