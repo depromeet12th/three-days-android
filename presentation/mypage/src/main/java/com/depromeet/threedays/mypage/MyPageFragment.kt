62f45e95f2d0ca39f9e7a2b0ac1cfb6bf7c7a8f6
Haeseong Jeon <seong0428@gmail.com>
2022-12-11 08:43:27 +0900

    feat: nickname 수정 dialog 추가

--- presentation/mypage/src/main/java/com/depromeet/threedays/mypage/MyPageFragment.kt
+++ presentation/mypage/src/main/java/com/depromeet/threedays/mypage/MyPageFragment.kt
@@ -1,12 +1,31 @@
 package com.depromeet.threedays.mypage
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.depromeet.threedays.core.BaseFragment
 import com.depromeet.threedays.mypage.databinding.FragmentMyPageBinding
+import com.depromeet.threedays.mypage.nickname.EditNicknameDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyPageFragment: BaseFragment<FragmentMyPageBinding, MyPageViewModel>(R.layout.fragment_my_page) {
     override val viewModel by viewModels<MyPageViewModel>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initEvent()
+    }
+
+    private fun initEvent() {
+        binding.ivEdit.setOnClickListener {
+            onEditButtonClicked()
+        }
+    }
+
+    private fun onEditButtonClicked() {
+        val nickname = binding.tvNickname.text.toString()
+        val dialog = EditNicknameDialog(requireContext(), nickname)
+        dialog.show()
+    }
 }
