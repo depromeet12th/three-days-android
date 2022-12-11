858071f03e9fe1becb88b859cfa2d33635feb2cd
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-12 02:50:42 +0900

    feat: Activity와 ViewModel 생성

---
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/share/ShareMateViewModel.kt
@@ -0,0 +1,12 @@
+package com.depromeet.threedays.mate.share
+
+import com.depromeet.threedays.core.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ShareMateViewModel @Inject constructor(
+
+) : BaseViewModel() {
+
+}
