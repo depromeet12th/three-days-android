27a6d4e0a799e890b291d858f6a7d9ad83832822
Juhwan Kim <juhwan.dev@gmail.com>
2022-12-15 23:48:53 +0900

    DEP-202 ui: 최종 레벨 달성 (#68)
    * ui: 사용할 아이콘, selector, 문자열 추가
    
    * ui: 최종 레벨 달성 팝업 구현
    
    * chore: mate에 구현했던 다이얼로그를 util로 이동

---
+++ core/src/main/java/com/depromeet/threedays/core/util/ThreeDaysNoButtonDialogFragment.kt
@@ -0,0 +1,56 @@
+package com.depromeet.threedays.core.util
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.depromeet.threedays.core.R
+import com.depromeet.threedays.core.databinding.FragmentThreeDaysNoButtonDialogBinding
+import com.depromeet.threedays.core.setOnSingleClickListener
+
+class ThreeDaysNoButtonDialogFragment(val resId: Int, val content: String) : DialogFragment() {
+    private var _binding: FragmentThreeDaysNoButtonDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_three_days_no_button_dialog,
+            container,
+            false
+        )
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setLayout()
+    }
+
+    private fun setLayout() {
+        dialog?.window?.run {
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        }
+        binding.ivIllustrator.setImageResource(resId)
+        binding.tvContent.text = content
+        binding.ivClose.setOnSingleClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
