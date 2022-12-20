e5ed9c63c34b1ca71f221164ba8261de641ed84b
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-21 03:55:24 +0900

    feat: 나의 기록 습관 리스트 불러오는 api 연동

--- presentation/history/src/main/java/com/depromeet/threedays/history/HistoryFragment.kt
+++ presentation/history/src/main/java/com/depromeet/threedays/history/HistoryFragment.kt
@@ -5,23 +5,28 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.depromeet.threedays.core.BaseFragment
 import com.depromeet.threedays.core.setOnSingleClickListener
 import com.depromeet.threedays.create.create.HabitCreateActivity
 import com.depromeet.threedays.history.databinding.FragmentHistoryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
 @AndroidEntryPoint
 class HistoryFragment: BaseFragment<FragmentHistoryBinding, HistoryViewModel>(R.layout.fragment_history) {
     override val viewModel by viewModels<HistoryViewModel>()
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
         initEvent()
+        setObserve()
     }
 
     private fun initView() {
@@ -42,6 +47,17 @@ class HistoryFragment: BaseFragment<FragmentHistoryBinding, HistoryViewModel>(R.
 
         binding.btnCreateHabit.setOnSingleClickListener {
             startActivity(Intent(requireActivity(), HabitCreateActivity::class.java))
+        }
+    }
+
+    private fun setObserve() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.habits.collect {
+                    binding.groupNoHabit.isVisible = it.isEmpty()
+                    binding.ncvHasHabit.isVisible = it.isNotEmpty()
+                }
+            }
         }
     }
 }
