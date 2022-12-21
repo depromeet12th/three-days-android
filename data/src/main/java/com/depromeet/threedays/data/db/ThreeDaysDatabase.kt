f13c74e93439a0e9287c95afa58adcb70356ff13
Haeseong Jeon <seong0428@gmail.com>
2022-12-21 18:55:30 +0900

    refactor: 안쓰는 코드 삭제

--- data/src/main/java/com/depromeet/threedays/data/db/ThreeDaysDatabase.kt
+++
@@ -1,8 +0,0 @@
-package com.depromeet.threedays.data.db
-
-//const val DATABASE_NAME_THREE_DAYS = "THREE_DAYS"
-//
-//@Database(entities = [GoalEntity::class], version = 2)
-//abstract class ThreeDaysDatabase: RoomDatabase() {
-//    abstract fun goalDao(): GoalDao
-//}
