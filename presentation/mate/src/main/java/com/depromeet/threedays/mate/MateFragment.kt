79406502e147628ad1f0701a392eca8c7175fe44
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-22 02:17:55 +0900

    feat: 짝꿍 유무에 따라 나뉘던 fragment 통합

--- presentation/mate/src/main/java/com/depromeet/threedays/mate/MateFragment.kt
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/MateFragment.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.depromeet.threedays.core.BaseFragment
 import com.depromeet.threedays.core.setOnSingleClickListener
 import com.depromeet.threedays.mate.databinding.FragmentMateBinding
+import com.depromeet.threedays.navigator.ConnectHabitNavigator
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MateFragment: BaseFragment<FragmentMateBinding, MateViewModel>(R.layout.fragment_mate) {
     override val viewModel by viewModels<MateViewModel>()
+    
+    @Inject
+    lateinit var connectHabitNavigator: ConnectHabitNavigator
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initEvent()
+        setObserve()
     }
 
     private fun initEvent() {
@@ -29,5 +39,25 @@ class MateFragment: BaseFragment<FragmentMateBinding, MateViewModel>(R.layout.fr
                 }
             }
         }
+        binding.btnCreateMate.setOnSingleClickListener {
+            startActivity(connectHabitNavigator.intent(requireContext()))
+        }
+    }
+
+    private fun setObserve() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    showMateOrDefaultView(it.hasMate)
+                }
+            }
+        }
+    }
+
+    private fun showMateOrDefaultView(hasMate: Boolean) {
+        binding.groupHasMate.isVisible = hasMate
+        binding.groupNoHabit.isVisible = hasMate.not()
+        binding.groupSpeechBubble.isVisible = hasMate
+        binding.clBottomSheet.isVisible = hasMate
     }
 }
