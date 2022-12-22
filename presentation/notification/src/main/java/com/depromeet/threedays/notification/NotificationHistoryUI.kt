9e7350f154ce5a807ed90d765f657848372a683f
Haeseong Jeon <seong0428@gmail.com>
2022-12-23 01:21:35 +0900

    feat: 날짜 표기 방법 적용

--- presentation/notification/src/main/java/com/depromeet/threedays/notification/NotificationHistoryUI.kt
+++ presentation/notification/src/main/java/com/depromeet/threedays/notification/NotificationHistoryUI.kt
@@ -2,12 +2,14 @@ package com.depromeet.threedays.notification
 
 import com.depromeet.threedays.domain.entity.notification.NotificationHistory
 import com.depromeet.threedays.domain.entity.notification.NotificationHistoryStatus
+import java.time.LocalDateTime
 
 data class NotificationHistoryUI(
     val id: Long,
     val title: String,
     val content: String,
     var status: NotificationHistoryStatus,
+    val createdAt: LocalDateTime,
 ) {
     companion object {
         fun from(notificationHistory: NotificationHistory) = NotificationHistoryUI(
@@ -15,6 +17,7 @@ data class NotificationHistoryUI(
             title = notificationHistory.title,
             content = notificationHistory.content,
             status = notificationHistory.status,
+            createdAt = notificationHistory.createdAt,
         )
     }
 
@@ -26,6 +29,7 @@ data class NotificationHistoryUI(
             title = this.title,
             content = this.content,
             status = status ?: this.status,
+            createdAt = this.createdAt,
         )
     }
 }
--- presentation/notification/src/main/java/com/depromeet/threedays/notification/NotificationHistoryViewHolder.kt
+++ presentation/notification/src/main/java/com/depromeet/threedays/notification/NotificationHistoryViewHolder.kt
@@ -3,9 +3,11 @@ package com.depromeet.threedays.notification
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.depromeet.threedays.core.extensions.formatRelatively
+import com.depromeet.threedays.domain.entity.notification.NotificationHistoryStatus
 import com.depromeet.threedays.notification.databinding.ItemNotificationBinding
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import com.depromeet.threedays.core_design_system.R as CoreDesignSystemResources
 
 class NotificationHistoryViewHolder(
     private val binding: ItemNotificationBinding,
@@ -16,8 +18,14 @@ class NotificationHistoryViewHolder(
         // 데이터 연결해주는 작업
         binding.tvNotificationTitle.text = notificationHistoryUI.title
         binding.tvNotificationContent.text = notificationHistoryUI.content
-        binding.tvTime.text = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
-
+        binding.tvTime.text = notificationHistoryUI.createdAt.formatRelatively(LocalDateTime.now())
+        binding.clNotificationItem.setBackgroundResource(
+            if (notificationHistoryUI.status == NotificationHistoryStatus.CHECKED) {
+                CoreDesignSystemResources.color.white
+            } else {
+                CoreDesignSystemResources.color.gray_100
+            }
+        )
         initEvent(notificationHistoryUI = notificationHistoryUI)
     }
 
