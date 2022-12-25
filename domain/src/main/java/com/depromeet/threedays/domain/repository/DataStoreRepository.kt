cce5cd13a3ed69070f95156e76e937b3b744716a
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-26 03:20:35 +0900

    chore: 필요하지 않은 파일 삭제

--- domain/src/main/java/com/depromeet/threedays/domain/repository/DataStoreRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.depromeet.threedays.domain.repository
-
-interface DataStoreRepository {
-    suspend fun readDataStore(key: String): String?
-    suspend fun writeDataStore(key: String, value: String)
-    suspend fun resetDataStore()
-}
