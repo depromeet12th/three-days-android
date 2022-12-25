ab44337ae4029a42c10a2f84e6798813089f2830
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-26 00:12:09 +0900

    feat: 스크린샷 저장 기능 구현

--- presentation/mate/src/main/java/com/depromeet/threedays/mate/share/ShareMateActivity.kt
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/share/ShareMateActivity.kt
@@ -1,16 +1,28 @@
 package com.depromeet.threedays.mate.share
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.depromeet.threedays.core.BaseActivity
 import com.depromeet.threedays.core.setOnSingleClickListener
+import com.depromeet.threedays.core.util.ThreeDaysToast
 import com.depromeet.threedays.mate.R
 import com.depromeet.threedays.mate.databinding.ActivityShareMateBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.util.*
 
 @AndroidEntryPoint
 class ShareMateActivity : BaseActivity<ActivityShareMateBinding>(R.layout.activity_share_mate) {
@@ -30,7 +42,7 @@ class ShareMateActivity : BaseActivity<ActivityShareMateBinding>(R.layout.activi
             finish()
         }
         binding.tvImageSave.setOnSingleClickListener {
-
+            saveScreenShot(binding.clScreenShotArea)
         }
     }
 
@@ -49,5 +61,48 @@ class ShareMateActivity : BaseActivity<ActivityShareMateBinding>(R.layout.activi
 
     private fun setScreenShotBackgroundColor(resId: Int) {
         binding.clScreenShotArea.setBackgroundResource(resId)
+    }
+
+    // 특정 레이아웃 캡쳐해서 저장하기
+    fun saveScreenShot(view: View?) {
+        val title = Date() // 파일명 중복 방지를 위해 사용될 현재시간
+
+        if (view == null) { // Null Point Exception ERROR 방지
+            println("::::ERROR:::: view == NULL")
+            return
+        }
+
+        /* 캡쳐 파일 저장 */
+        view.buildDrawingCache() //캐시 비트 맵 만들기
+        val bitmap = view.drawingCache
+
+        var fos: OutputStream? = null
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            this.contentResolver?.also { resolver ->
+
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, "$title.png")
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, "$title.png")
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
+            ThreeDaysToast().show(
+                this,
+                resources.getString(R.string.save_screen_shot_success)
+            )
+            finish()
+        }
     }
 }
