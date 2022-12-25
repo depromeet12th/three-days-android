81e31ef4a44e5046862dce701dbf0695b2e75e74
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-25 23:58:01 +0900

    feat: 습관 정보를 가져와 공유하기 페이지에 연동 구현

--- presentation/mate/src/main/java/com/depromeet/threedays/mate/share/ShareMateActivity.kt
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/share/ShareMateActivity.kt
@@ -2,11 +2,15 @@ package com.depromeet.threedays.mate.share
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.depromeet.threedays.core.BaseActivity
 import com.depromeet.threedays.core.setOnSingleClickListener
 import com.depromeet.threedays.mate.R
 import com.depromeet.threedays.mate.databinding.ActivityShareMateBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ShareMateActivity : BaseActivity<ActivityShareMateBinding>(R.layout.activity_share_mate) {
@@ -15,8 +19,35 @@ class ShareMateActivity : BaseActivity<ActivityShareMateBinding>(R.layout.activi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val habitId = intent.getLongExtra("habitId", -1)
+        viewModel.fetchMate(habitId)
+        initEvent()
+        setObserve()
+    }
+
+    private fun initEvent() {
         binding.ivClose.setOnSingleClickListener {
             finish()
         }
+        binding.tvImageSave.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun setObserve() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    if(it.singleHabit != null) {
+                        binding.singleHabit = it.singleHabit
+                    }
+                    setScreenShotBackgroundColor(it.backgroundResId)
+                }
+            }
+        }
+    }
+
+    private fun setScreenShotBackgroundColor(resId: Int) {
+        binding.clScreenShotArea.setBackgroundResource(resId)
     }
 }
