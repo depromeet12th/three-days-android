ae4e8986f992f033aeca67e82b80b83725d8e5d4
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-25 23:56:02 +0900

    feat: Mate api 수정 사항 반영

--- presentation/mate/src/main/java/com/depromeet/threedays/mate/MateFragment.kt
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/MateFragment.kt
@@ -1,5 +1,6 @@
 package com.depromeet.threedays.mate
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
@@ -11,6 +12,7 @@ import com.depromeet.threedays.core.BaseFragment
 import com.depromeet.threedays.core.setOnSingleClickListener
 import com.depromeet.threedays.mate.create.step1.model.MateUI
 import com.depromeet.threedays.mate.databinding.FragmentMateBinding
+import com.depromeet.threedays.mate.share.ShareMateActivity
 import com.depromeet.threedays.navigator.ConnectHabitNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -43,6 +45,11 @@ class MateFragment: BaseFragment<FragmentMateBinding, MateViewModel>(R.layout.fr
         binding.btnCreateMate.setOnSingleClickListener {
             startActivity(connectHabitNavigator.intent(requireContext()))
         }
+        binding.ivShare.setOnSingleClickListener {
+            val intent = Intent(requireActivity(), ShareMateActivity::class.java)
+            intent.putExtra("habitId", viewModel.uiState.value.mate?.habitId)
+            startActivity(intent)
+        }
     }
 
     private fun setObserve() {
@@ -50,7 +57,7 @@ class MateFragment: BaseFragment<FragmentMateBinding, MateViewModel>(R.layout.fr
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.uiState.collect {
                     showMateOrDefaultView(hasMate = it.hasMate)
-                    setMateInfo(mateUI = it.mate) // TODO: api가 수정되면 습관과 연결된 짝꿍으로 변경할 것
+                    setMateInfo(mateUI = it.mate)
                 }
             }
         }
@@ -63,14 +70,12 @@ class MateFragment: BaseFragment<FragmentMateBinding, MateViewModel>(R.layout.fr
         binding.clBottomSheet.isVisible = hasMate
     }
 
-    private fun setMateInfo(mateUI: List<MateUI?>) { // TODO: api가 수정되면 습관과 연결된 짝꿍으로 변경할 것
-        if(mateUI.isNotEmpty()) {
-            mateUI[0]?.let {
-                binding.tvSpeechBubble.text = it.bubble
-                binding.tvLevel.text = getString(R.string.level, it.level)
-                binding.tvMateNickname.text = it.title
-                binding.tvStartDate.text = getString(R.string.start_date_with_mate, it.createAt.toString().substring(0, 10).replace("-", ".")) // TODO: api가 수정되면 습관과 연결된 짝꿍으로 변경할 것
-            }
+    private fun setMateInfo(mateUI: MateUI?) {
+        mateUI?.let {
+            binding.tvSpeechBubble.text = it.bubble
+            binding.tvLevel.text = getString(R.string.level, it.level)
+            binding.tvMateNickname.text = it.title
+            binding.tvStartDate.text = getString(R.string.start_date_with_mate, it.createAt.toString().substring(0, 10).replace("-", "."))
         }
     }
 }
