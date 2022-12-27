459942a84ef905920b0ee19bbb2c443d40fa4f99
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-28 08:12:17 +0900

    feat: 짝꿍 만들기 2단계에서 선택에 따라 상자 모양이 달라지도록 구현

--- presentation/mate/src/main/java/com/depromeet/threedays/mate/create/step2/ChooseMateTypeViewModel.kt
+++ presentation/mate/src/main/java/com/depromeet/threedays/mate/create/step2/ChooseMateTypeViewModel.kt
@@ -1,7 +1,9 @@
 package com.depromeet.threedays.mate.create.step2
 
 import com.depromeet.threedays.core.BaseViewModel
+import com.depromeet.threedays.domain.entity.Color
 import com.depromeet.threedays.mate.R
+import com.depromeet.threedays.mate.create.step1.model.HabitUI
 import com.depromeet.threedays.core_design_system.R as core_design
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
@@ -16,6 +18,8 @@ class ChooseMateTypeViewModel @Inject constructor(
     val uiState: StateFlow<UiState>
         get() = _uiState
 
+    lateinit var habit: HabitUI
+
     fun setMateType(mateType: MateType) {
         when(mateType) {
             MateType.WhippingMate -> {
@@ -25,6 +29,11 @@ class ChooseMateTypeViewModel @Inject constructor(
                         carrotMateBackgroundRes = core_design.drawable.bg_rect_gray100_r10,
                         whippingMateRes = R.drawable.ic_whipping_mate_selected,
                         carrotMateRes = R.drawable.ic_carrot_mate_unselected,
+                        boxImageResId = when(habit.color) {
+                            Color.GREEN -> R.drawable.bg_box_mate_whip_green
+                            Color.BLUE -> R.drawable.bg_box_mate_whip_blue
+                            Color.PINK -> R.drawable.bg_box_mate_whip_pink
+                        }
                     )
                 }
             }
@@ -35,9 +44,27 @@ class ChooseMateTypeViewModel @Inject constructor(
                         carrotMateBackgroundRes = R.drawable.bg_rect_gray_200_border_gray_400_r10,
                         whippingMateRes = R.drawable.ic_whipping_mate_unselected,
                         carrotMateRes = R.drawable.ic_carrot_mate_selected,
+                        boxImageResId = when(habit.color) {
+                            Color.GREEN -> R.drawable.bg_box_mate_carrot_green
+                            Color.BLUE -> R.drawable.bg_box_mate_carrot_blue
+                            Color.PINK -> R.drawable.bg_box_mate_carrot_pink
+                        }
                     )
                 }
             }
+        }
+    }
+
+    fun setClickHabit(clickedHabit: HabitUI) {
+        habit = clickedHabit
+        _uiState.update {
+            it.copy(
+                boxImageResId = when(habit.color) {
+                    Color.GREEN -> R.drawable.bg_box_mate_whip_green
+                    Color.BLUE -> R.drawable.bg_box_mate_whip_blue
+                    Color.PINK -> R.drawable.bg_box_mate_whip_pink
+                }
+            )
         }
     }
 }
@@ -48,6 +75,7 @@ data class UiState(
     val carrotMateBackgroundRes: Int = core_design.drawable.bg_rect_gray200_r10,
     val whippingMateRes: Int = R.drawable.ic_whipping_mate_selected,
     val carrotMateRes: Int = R.drawable.ic_carrot_mate_unselected,
+    val boxImageResId: Int = R.drawable.bg_box_mate_default,
 )
 
 sealed interface MateType {
