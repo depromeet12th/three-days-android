e3af3d61c6655c2175792ef65525e4a10b84d13e
juhwankim-dev <juhwan.dev@gmail.com>
2022-12-30 03:04:47 +0900

    fix: 가장 많이 실천한 습관이 뜨지 않는 버그 수정

--- data/src/main/java/com/depromeet/threedays/data/entity/record/RecordEntity.kt
+++ data/src/main/java/com/depromeet/threedays/data/entity/record/RecordEntity.kt
@@ -1,17 +1,18 @@
 package com.depromeet.threedays.data.entity.record
 
+import com.depromeet.threedays.domain.entity.record.FrequentHabit
 import com.depromeet.threedays.domain.entity.record.Record
 
 data class RecordEntity(
     val achievementCount: Int,
-    val frequentHabitEntity: FrequentHabitEntity?,
+    val frequentHabit: FrequentHabit?,
     val rewardCount: Int
 )
 
 fun RecordEntity.toDomainModel(): Record {
     return Record(
         achievementCount = this.achievementCount,
-        frequentHabit = this.frequentHabitEntity?.toDomainModel(),
+        frequentHabit = this.frequentHabit,
         rewardCount = this.rewardCount,
     )
 }
