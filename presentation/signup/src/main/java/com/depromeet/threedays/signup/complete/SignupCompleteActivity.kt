a166496e28ffdb7d9e0207294fc48e275e20aad2
hyein <hyein2072@gmail.com>
2023-01-08 16:04:31 +0900

    feat: 첫 회원가입 시, 회원가입 완료 화면으로 이동

--- presentation/signup/src/main/java/com/depromeet/threedays/signup/complete/SignupCompleteActivity.kt
+++ presentation/signup/src/main/java/com/depromeet/threedays/signup/complete/SignupCompleteActivity.kt
@@ -1,12 +1,30 @@
 package com.depromeet.threedays.signup.complete
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.depromeet.threedays.core.BaseActivity
+import com.depromeet.threedays.core.util.setOnSingleClickListener
+import com.depromeet.threedays.navigator.HomeNavigator
 import com.depromeet.threedays.signup.R
+import com.depromeet.threedays.signup.databinding.ActivitySignupCompleteBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class SignupCompleteActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class SignupCompleteActivity : BaseActivity<ActivitySignupCompleteBinding>(R.layout.activity_signup_complete) {
+
+    @Inject
+    lateinit var homeNavigator: HomeNavigator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_complete)
+        initView()
+    }
+
+    private fun initView() {
+        binding.tvCheck.setOnSingleClickListener {
+            val intent = homeNavigator.intent(this)
+            startActivity(intent)
+            finish()
+        }
     }
 }
