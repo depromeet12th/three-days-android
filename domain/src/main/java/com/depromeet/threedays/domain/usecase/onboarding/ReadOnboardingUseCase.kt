080a5adb9f5ffa74b9a1c47a0233d87620dc4cae
juhwankim-dev <juhwan.dev@gmail.com>
2023-01-08 19:25:49 +0900

    refactor: 불필요한 when절 제거

--- domain/src/main/java/com/depromeet/threedays/domain/usecase/onboarding/ReadOnboardingUseCase.kt
+++ domain/src/main/java/com/depromeet/threedays/domain/usecase/onboarding/ReadOnboardingUseCase.kt
@@ -6,13 +6,7 @@ import javax.inject.Inject
 
 class ReadOnboardingUseCase @Inject constructor(val repository: OnboardingRepository) {
     suspend fun execute(onboardingType: OnboardingType): String? {
-        val key = when (onboardingType) {
-            OnboardingType.NOTIFICATION_RECOMMEND -> OnboardingType.NOTIFICATION_RECOMMEND.key
-            OnboardingType.AFTER_SPLASH -> OnboardingType.AFTER_SPLASH.key
-            OnboardingType.MATE -> OnboardingType.MATE.key
-            OnboardingType.ARCHIVED_HABIT -> OnboardingType.ARCHIVED_HABIT.key
-        }
-        return repository.readOnboardnig(key)
+        return repository.readOnboardnig(onboardingType.key)
     }
 }
 
--- domain/src/main/java/com/depromeet/threedays/domain/usecase/onboarding/WriteOnboardingUseCase.kt
+++ domain/src/main/java/com/depromeet/threedays/domain/usecase/onboarding/WriteOnboardingUseCase.kt
@@ -6,12 +6,6 @@ import javax.inject.Inject
 
 class WriteOnboardingUseCase @Inject constructor(val repository: OnboardingRepository) {
     suspend fun execute(onboardingType: OnboardingType) {
-        val key = when (onboardingType) {
-            OnboardingType.NOTIFICATION_RECOMMEND -> OnboardingType.NOTIFICATION_RECOMMEND.key
-            OnboardingType.AFTER_SPLASH -> OnboardingType.AFTER_SPLASH.key
-            OnboardingType.MATE -> OnboardingType.MATE.key
-            OnboardingType.ARCHIVED_HABIT -> OnboardingType.ARCHIVED_HABIT.key
-        }
-        return repository.writeOnboardnig(key = key, value = "IS_SHWON")
+        return repository.writeOnboardnig(key = onboardingType.key, value = "IS_SHWON")
     }
 }
