2c536302f3cc703cd2771608af05f1c644b98277
hyein <hyein2072@gmail.com>
2023-01-08 15:24:45 +0900

    ui: 회원가입 완료 화면 제작

---
+++ presentation/signup/src/main/java/com/depromeet/threedays/signup/complete/SignupCompleteActivity.kt
@@ -0,0 +1,12 @@
+package com.depromeet.threedays.signup.complete
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.depromeet.threedays.signup.R
+
+class SignupCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_complete)
+    }
+}
