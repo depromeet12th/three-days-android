ccb9e7e403deefaef45bcabe8acdfaa664fd43a2
hyein <hyein2072@gmail.com>
2023-01-08 14:39:16 +0900

    feat: 이미 회원가입 한 유저인지, 신규가입 유저인지 판단하는 로직 추가

--- data/src/main/java/com/depromeet/threedays/data/mapper/MemberMapper.kt
+++ data/src/main/java/com/depromeet/threedays/data/mapper/MemberMapper.kt
@@ -1,10 +1,12 @@
 package com.depromeet.threedays.data.mapper
 
 import com.depromeet.threedays.data.entity.auth.SignupMemberEntity
+import com.depromeet.threedays.data.entity.base.ApiResponse
 import com.depromeet.threedays.data.entity.member.MemberEntity
 import com.depromeet.threedays.domain.entity.auth.SignupMember
 import com.depromeet.threedays.domain.entity.member.AuthenticationProvider
 import com.depromeet.threedays.domain.entity.member.Member
+import com.depromeet.threedays.domain.key.RESOURCE_CREATE
 
 fun MemberEntity.toMember() = Member(
     memberId = this.id,
@@ -14,11 +16,16 @@ fun MemberEntity.toMember() = Member(
     resource = this.resource,
 )
 
-fun SignupMemberEntity.toSignupMember() = SignupMember(
-    id = this.id,
-    name = this.name,
-    certificationSubject = AuthenticationProvider.from(this.certificationSubject),
-    token = this.token,
-    resource = this.resource,
-    notificationConsent = this.notificationConsent
-)
+fun ApiResponse<SignupMemberEntity>.toSignupMember(): SignupMember {
+    val data = this.data ?: throw IllegalStateException()
+    val code = this.code
+    return SignupMember(
+        id = data.id,
+        name = data.name,
+        certificationSubject = AuthenticationProvider.from(data.certificationSubject),
+        token = data.token,
+        resource = data.resource,
+        notificationConsent = data.notificationConsent,
+        isSignedUp = (code != RESOURCE_CREATE)
+    )
+}
