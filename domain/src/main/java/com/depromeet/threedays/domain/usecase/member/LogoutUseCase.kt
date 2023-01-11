21007be00f64213af3472204fcd1302820c237d5
Haeseong Jeon <seong0428@gmail.com>
2023-01-12 03:26:21 +0900

    fix: 로그아웃, 회원탈퇴시 앱에 저장된 데이터 모두 삭제

--- domain/src/main/java/com/depromeet/threedays/domain/usecase/member/LogoutUseCase.kt
+++ domain/src/main/java/com/depromeet/threedays/domain/usecase/member/LogoutUseCase.kt
@@ -2,6 +2,7 @@ package com.depromeet.threedays.domain.usecase.member
 
 import com.depromeet.threedays.domain.repository.AuthRepository
 import com.depromeet.threedays.domain.repository.DeviceUniqueIdRepository
+import com.depromeet.threedays.domain.repository.LocalDataRepository
 import com.depromeet.threedays.domain.repository.MemberRepository
 import javax.inject.Inject
 
@@ -9,12 +10,13 @@ class LogoutUseCase @Inject constructor(
     private val memberRepository: MemberRepository,
     private val deviceUniqueIdRepository: DeviceUniqueIdRepository,
     private val authRepository: AuthRepository,
+    private val localDataRepository: LocalDataRepository,
 ) {
     suspend operator fun invoke() {
         deviceUniqueIdRepository.findOne()?.let { deviceId ->
             memberRepository.logout(deviceId = deviceId)
         }
         authRepository.removeTokensFromLocal()
-        // TODO: notification permission 삭제
+        localDataRepository.removeAll()
     }
 }
--- domain/src/main/java/com/depromeet/threedays/domain/usecase/member/WithdrawUseCase.kt
+++ domain/src/main/java/com/depromeet/threedays/domain/usecase/member/WithdrawUseCase.kt
@@ -1,16 +1,18 @@
 package com.depromeet.threedays.domain.usecase.member
 
 import com.depromeet.threedays.domain.repository.AuthRepository
+import com.depromeet.threedays.domain.repository.LocalDataRepository
 import com.depromeet.threedays.domain.repository.MemberRepository
 import javax.inject.Inject
 
 class WithdrawUseCase @Inject constructor(
     private val memberRepository: MemberRepository,
     private val authRepository: AuthRepository,
+    private val localDataRepository: LocalDataRepository,
 ) {
     suspend operator fun invoke() {
         memberRepository.withdraw()
         authRepository.removeTokensFromLocal()
-        // TODO: notification permission 삭제
+        localDataRepository.removeAll()
     }
 }
