04bd7ce2963ddba25af4995d6d74e0b5d9ce649b
Haeseong Jeon <seong0428@gmail.com>
2023-01-12 03:23:11 +0900

    fix: 로그아웃시 올바른 deviceId 추가

--- domain/src/main/java/com/depromeet/threedays/domain/usecase/member/LogoutUseCase.kt
+++ domain/src/main/java/com/depromeet/threedays/domain/usecase/member/LogoutUseCase.kt
@@ -1,17 +1,19 @@
 package com.depromeet.threedays.domain.usecase.member
 
 import com.depromeet.threedays.domain.repository.AuthRepository
+import com.depromeet.threedays.domain.repository.DeviceUniqueIdRepository
 import com.depromeet.threedays.domain.repository.MemberRepository
 import javax.inject.Inject
 
 class LogoutUseCase @Inject constructor(
     private val memberRepository: MemberRepository,
+    private val deviceUniqueIdRepository: DeviceUniqueIdRepository,
     private val authRepository: AuthRepository,
 ) {
     suspend operator fun invoke() {
-        // TODO: deviceId 조회 기능 추가
-        val deviceId = "identificationKey"
-        memberRepository.logout(deviceId = deviceId)
+        deviceUniqueIdRepository.findOne()?.let { deviceId ->
+            memberRepository.logout(deviceId = deviceId)
+        }
         authRepository.removeTokensFromLocal()
         // TODO: notification permission 삭제
     }
