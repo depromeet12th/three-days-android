d05b9188aab8c613b1e79e10c8efeb90b830e912
hyein <hyein2072@gmail.com>
2023-01-12 23:14:12 +0900

    fix: call에서 body 변환이 제대로 이뤄지지 않았던 오류 해결

--- data/src/main/java/com/depromeet/threedays/data/api/exception/ResultCall.kt
+++ data/src/main/java/com/depromeet/threedays/data/api/exception/ResultCall.kt
@@ -1,11 +1,10 @@
 package com.depromeet.threedays.data.api.exception
 
-import com.depromeet.threedays.data.entity.base.ApiResponse
+import com.depromeet.threedays.data.entity.base.ErrorResponse
 import com.depromeet.threedays.domain.exception.ThreeDaysException
 import com.depromeet.threedays.domain.key.NO_INTERNET_CONNECTION
 import com.depromeet.threedays.domain.key.UNKNOWN_ERROR
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import okhttp3.Request
 import okio.Timeout
 import retrofit2.Call
@@ -14,17 +13,18 @@ import retrofit2.HttpException
 import retrofit2.Response
 import timber.log.Timber
 import java.io.IOException
-import java.lang.reflect.Type
 
 class ResultCall<T>(private val call: Call<T>, private val gson: Gson) : Call<Result<T>> {
 
     override fun enqueue(callback: Callback<Result<T>>) {
         call.enqueue(object : Callback<T> {
             override fun onResponse(call: Call<T>, response: Response<T>) {
-                val type: Type = object : TypeToken<ApiResponse<Any>>() {}.type
-                val body = gson.fromJson<ApiResponse<Any>>(response.body().toString(), type)
+                val jsonBody = gson.toJson(response.body())
+                val body = gson.fromJson(
+                    jsonBody,
+                    ErrorResponse::class.java
+                )
                 val code = body.code
-
                 if (response.isSuccessful) {
                     callback.onResponse(
                         this@ResultCall,
@@ -34,7 +34,7 @@ class ResultCall<T>(private val call: Call<T>, private val gson: Gson) : Call<Re
                         )
                     )
                 } else {
-                    Timber.tag("ResultCall - onResponse").e("code : $code / ${ThreeDaysException(code, HttpException(response))}")
+                    Timber.tag("ResultCall - onResponse").e("code: $code, error: ${ThreeDaysException(code, HttpException(response))}")
 
                     callback.onResponse(
                         this@ResultCall,
@@ -45,8 +45,6 @@ class ResultCall<T>(private val call: Call<T>, private val gson: Gson) : Call<Re
                         )
                     )
                 }
-
-
             }
 
             override fun onFailure(call: Call<T>, t: Throwable) {
